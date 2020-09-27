a6d86e0e6deb35fe4a571e6a24f3f3328647abe7
ManahariDahal <manahari.dahal1@gmail.com>
2020-09-26 22:44:55 -0600

    updated prove 2

--- routes/prove02.js
+++ routes/prove02.js
@@ -1,26 +1,20 @@
 const express = require('express');
 const router = express.Router();
-
 router.get('/',(req, res, next) => {
     res.render('pages/prove02/form', { 
-        // title: 'Prove Assignment 02', 
-        // path: '/prove02', // For pug, EJS 
-        // activeTA03: true, // For HBS
-        // contentCSS: true, // For HBS
+        
     });
 });
 
 router.post('/submit',(req, res, next) => {
     res.render('pages/prove02/display', { 
-        // title: 'Prove Assignment 02', 
-        // path: '/prove02', // For pug, EJS 
-        // activeTA03: true, // For HBS
-        // contentCSS: true, // For HBS
         title: req.body.title,
         summary: req.body.summary,
         date: req.body.date,
         authorName: req.body.authorName,
     });
+    res.redirect('pages/prove02/form.ejs');
 });
+
 
 module.exports = router;
